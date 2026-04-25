9ebf5dc8bab8f19f2b84d57109a8e5e1d7a9e43a
bogdan9876 <bogdankryveiko@gmail.com>
2026-04-26 02:29:49 +0300

    probably fix prisma to postgres

--- server/src/database/database.service.ts
+++ server/src/database/database.service.ts
@@ -1,9 +1,19 @@
 import { Injectable, Logger, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { Pool } from 'pg';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
   private readonly logger = new Logger(DatabaseService.name);
+  private readonly pool: Pool;
+
+  constructor() {
+    const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+    const adapter = new PrismaPg(pool);
+    super({ adapter });
+    this.pool = pool;
+  }
 
   async onModuleInit(): Promise<void> {
     await this.$connect();
@@ -12,6 +22,7 @@ export class DatabaseService extends PrismaClient implements OnModuleInit, OnMod
 
   async onModuleDestroy(): Promise<void> {
     await this.$disconnect();
+    await this.pool.end();
     this.logger.log('Database disconnected');
   }
 }
